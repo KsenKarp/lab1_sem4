b95502420d80d0f199f942ec99485c7696762338
KsenKarp <102474119+KsenKarp@users.noreply.github.com>
2023-05-26 15:29:33 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,11 +6,12 @@
 using namespace std;
 
 int main () {
-       string s;       
-	 system("./backwhile 0 1 2 3 > testwhile_withparams.txt");
-	 system("./backwhile > testwhile_noparams.txt");
-	 system ("./paramsbackwards 0 1 2 3 > testfor_withparams.txt");
-	 system("./paramsbackwards > testfor_noparams.txt");
+	int success_fl = true;
+        string s;       
+	system("./backwhile 0 1 2 3 > testwhile_withparams.txt");
+	system("./backwhile > testwhile_noparams.txt");
+	system ("./paramsbackwards 0 1 2 3 > testfor_withparams.txt");
+	system("./paramsbackwards > testfor_noparams.txt");
 	ifstream while_params("testwhile_withparams.txt");
 	ifstream while_noparams("testwhile_noparams.txt");
 	ifstream for_params("testfor_withparams.txt");
@@ -19,17 +20,24 @@ int main () {
 	string no_param = "No parameters";
 	while(getline(while_params, s)) {
 		int param = stoi(s);
-		assert(while_count == param);
+		if (assert(while_count == param) == false) success_fl = false;
 		while_count--;
 	}
+	if (success_fl) cout << "test_while passed" << endl;
+	else cout << "test_while failed" << endl;
+	success_fl = true;
 	while(getline(for_params, s)) {
 		 int param = stoi(s);
-		 assert(for_count == param);
+		 if (assert(for_count == param) == false) success_fl = false;
 		 for_count--;
 	}
+	if (success_fl) cout << "test_for passed" << endl;
+	else cout << "test_for failed" << endl;
 	getline(while_noparams, s);
-	assert(no_param == s);
+	if (assert(no_param == s)) cout << "test_while with 0 param passed" << endl;
+	else cout << "test_while with 0 param failed" << endl;
 	getline(for_noparams, s);
-	assert(no_param == s);
+	if (assert(no_param == s)) cout << "test_for with 0 param passed" << endl;
+	else cout << "test_for with 0 param failed" << endl;
 	return 0;
 }
